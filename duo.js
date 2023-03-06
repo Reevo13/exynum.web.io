5ad0cc27aeffcd2ec315b4879a47fe25c3397c38
Reevo13 <121067601+Reevo13@users.noreply.github.com>
2023-03-06 22:00:54 +0700

    Add files via upload

---
+++ duo.js
@@ -0,0 +1,41 @@
+const body = document.body
+const btn1 = document.getElementById('btn1')
+const nv = document.getElementById('navbar')
+
+btn1.img = 'exe.png'
+console.log('aman')
+
+
+
+
+
+
+body.style.background ='black'
+
+function ft() {
+  console.log('guaaman')
+  nv.style.display = 'block'
+}
+
+function bck() {
+  navbar.style.display = 'none'
+}
+
+function ml() {
+  cs.style.display = 'block'
+setTimeout(function() {
+    cs.style.display = 'none'
+   }, 3000);
+}
+function ff() {
+  cs.style.display = 'block'
+setTimeout(function() {
+    cs.style.display = 'none'
+   }, 3000);
+}
+function gs() {
+  cs.style.display = 'block'
+  setTimeout(function() {
+    cs.style.display = 'none'
+   }, 3000);
+}
